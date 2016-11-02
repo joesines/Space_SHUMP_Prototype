70f8a87af325cfbad9b75ebc0a945badd5b5e76d
shavanya215 <shavanya.215@gmail.com>
2016-11-01 20:55:51 -0400

    power up

---
+++ Assets/_Scripts/PowerUp.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour {
+
+        //This is an unusual but handy use of vector2s. xhold a min value 
+        //and y a max value fro a random.Range() that will be called later
+        public Vector2            rotMinMax = new Vector2(15, 90);
+        public Vector2           driftMinMax = new Vector2(.25f, 2);
+        public float             lifeTime = 6f;//Seconds the PowerUp Exists
+        public float            fadeTime = 4f;//Seconds it will then fade
+        public bool             _____________________;
+        public WeaponType       type;
+        public GameObject       cube;
+        public TextMesh         letter;
+        public Vector3          rotPerSeconds;//Euler rotation speed
+        public float            birthTime;
+	}
+
+void Awake() {
+    //fine the cube reference 
+    cube = transform.FindObjectOfType("Cube").gameObject;
+
+
+}
+	
+
+	
+	}
+
